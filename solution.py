55b4964b6b796fbfeef5498cd4cb04d56a304cfa
DheerajMadda <50489165+DheerajMadda@users.noreply.github.com>
2022-01-08 17:38:04 +0530

    Add files via upload

--- solution.py
+++ solution.py
@@ -67,7 +67,6 @@ def textify(y, labels):
     text_label = str(percent) + 2*" " + labels[np.argmax(y)]
     return text_label, text_color
 
-
 if __name__ == '__main__':
 
     # read command line arguments
@@ -77,7 +76,7 @@ if __name__ == '__main__':
     parser.add_argument("-m", "--model", help="Path to the model")
     args = parser.parse_args()
     
-    source = args.source if args.source else os.path.join(os.getcwd(), 'data', '2.mp4')
+    source = args.source if args.source else os.path.join(os.getcwd(), 'data', 'sample.mp4')
     label_file = args.labelpath if args.labelpath else os.path.join(os.getcwd(), 'data', 'imagenet_slim_labels.txt')
     model_file = args.model if args.model else os.path.join(os.getcwd(), 'data', 'inception_v3_2016_08_28_frozen.pb')
 
@@ -100,6 +99,9 @@ if __name__ == '__main__':
     input_operation = graph.get_operation_by_name(input_name)
     output_operation = graph.get_operation_by_name(output_name)
 
+    # create session
+    sess = tf.compat.v1.Session(graph=graph)
+
     # Read labels
     labels = load_labels(label_file)
 
@@ -107,7 +109,7 @@ if __name__ == '__main__':
     H = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     W = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
+            
     for frame_idx in range(total_frames):
         ret, frame = cap.read()
         x = read_tensor_from_image_file(
@@ -116,9 +118,8 @@ if __name__ == '__main__':
             input_width=input_width,
             input_mean=input_mean,
             input_std=input_std)
-    
-        with tf.compat.v1.Session(graph=graph) as sess:
-            results = sess.run(output_operation.outputs[0], {
+
+        results = sess.run(output_operation.outputs[0], {
                 input_operation.outputs[0]: x
             })
 
@@ -132,3 +133,4 @@ if __name__ == '__main__':
     
     cap.release()
     cv2.destroyAllWindows()
+    sess.close()
